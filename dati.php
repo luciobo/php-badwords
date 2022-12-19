9185908b622947cdccddaf2cb93de526c0355784
luciobo <luciobo@icloud.com>
2022-12-19 16:55:52 +0100

    aggiunto php e htm, stasmpato tutto a video

--- dati.php
+++ dati.php
@@ -1,3 +1,49 @@
-<?php 
+<?php
+
+$nome = $_GET["nomeUtente"];
+$messaggio = $_GET["messaggio"];
+
+
+var_dump($nome);
+var_dump($messaggio);
+
+echo "Nome ricevuto:" . $nome . "<br>";
+echo "Messaggio ricevuto:" . $messaggio . "<br>";
+
+echo str_replace("scoppiare", "  --hai scritto una parolaccia--", "$messaggio");
+
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>vue-email-list</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</head>
+
+<body>
+
+<div class="text-center">
+    <h2 class="text-success">Nome Utente</h2>
+    <h4 class="text-danger"><?php echo $nome?></h4>
+    <h2 class="text-success">Messaggio Scritto</h2>
+    <h4 class="text-danger"><?php echo str_replace("scoppiare", "  --hai scritto una parolaccia--", "$messaggio") ?></h4>
+</div>
+
+    <!-- javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 ?>
 
@@ -17,12 +17,30 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 
 <body>
 
-    <div id="app"></div>
+
+    <div class="text-center py-3">
+        <h1>Il mio primo form in php</h1>
+    </div>
+    <div class="row justify-content-center py-3">
+        <div class="col-6">
+            <form action="dati.php" method="GET">
+                <legend>Contatti [GET]</legend>
+                <div class="mb-3">
+                    <label for="" class="form-label">Nome</label>
+                    <input type="text" class="form-control" name="nomeUtente">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">Messaggio</label>
+                    <textarea class="form-control" name="messaggio"></textarea>
+                </div>
+                <button class="btn btn-primary">Invia</button>
+            </form>
+        </div>
+    </div>
 
     <!-- javascript -->
     <script src="js/main.js"></script>
